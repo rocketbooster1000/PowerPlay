634800f350e0a42e6b2524924aa41fa57bac3516
rocketbooster1000 <chadpowers444@gmail.com>
2022-11-13 19:39:14 -0700

    TeamSample Update: telemetry.addData

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamSample.java
@@ -7,11 +7,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class TeamSample extends OpMode{
     @Override
     public void init() {
-
+        telemetry.addData("Start-up", ": Hello!");
     }
 
     @Override
     public void loop() {
-
+        boolean gamepadPressed = (gamepad1.left_stick_x != 0) && (gamepad1.left_stick_y != 0);
+        if (gamepadPressed){
+            telemetry.addData("Left y: ", gamepad1.left_stick_y);
+            telemetry.addData("Left x: ", gamepad1.left_stick_x);
+        }
     }
 }
