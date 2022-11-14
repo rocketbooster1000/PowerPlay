2ede429931e5c8c6b8f41c50e4ccc8051696ab09
rocketbooster1000 <chadpowers444@gmail.com>
2022-11-13 18:52:35 -0700

    Gradle Update, TeamSample created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamSample.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class TeamSample extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
