a618360e58257a4422a8a9852a5adbb3ecc5cd8f
ella-noel26 <ella.n.frandsen@gmail.com>
2023-02-15 17:38:52 -0700

    ella is super duper hot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -53,8 +53,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
+    public static double kV = 0.018196088299765307;
+    public static double kA = 0.003;
     public static double kStatic = 0;
 
     /*
